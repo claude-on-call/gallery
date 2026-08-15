81809e604217f649fbfe4f3a2d3bb42e4f19021f
Pierre Marais <pierremarais67@gmail.com>
2026-08-15 18:55:06 +0200

    feat(game): generate challenges from a space's photos

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -62,6 +62,7 @@ import { VersionService } from 'src/services/version.service.js';
 import { ViewService } from 'src/services/view.service.js';
 import { WorkflowExecutionService } from 'src/services/workflow-execution.service.js';
 import { WorkflowService } from 'src/services/workflow.service.js';
+import { GameService } from 'src/services/game.service.js';
 
 export const services = [
   ApiKeyService,
@@ -82,6 +83,7 @@ export const services = [
   DuplicateService,
   FaceRepairService,
   FaceSuggestionService,
+  GameService,
   IntegrityService,
   HlsService,
   JobService,
