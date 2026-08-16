c8ab48f13f5a6c56f662b7c3b23352463d57012e
Pierre Marais <pierremarais67@gmail.com>
2026-08-16 21:27:55 +0200

    feat(game): aggregate a space's daily scores over a month

--- server/test/medium.factory.ts
+++ server/test/medium.factory.ts
@@ -98,6 +98,7 @@ import { mockEnvData } from 'test/repositories/config.repository.mock.js';
 import { newTelemetryRepositoryMock } from 'test/repositories/telemetry.repository.mock.js';
 import { factory, newDate, newEmbedding, newUuid } from 'test/small.factory.js';
 import { automock, wait } from 'test/utils.js';
+import { GameRepository } from 'src/repositories/game.repository.js';
 
 export const testAssetsDir = resolve(import.meta.dirname, '../../e2e/test-assets');
 
@@ -637,6 +638,7 @@ const newRealRepository = <T extends BaseServiceDeps[number]>(key: T, db: Kysely
     case FaceRepairRepository:
     case FaceRepairScanRepository:
     case FacePersonVerdictRepository:
+    case GameRepository:
     case IntegrityRepository:
     case MemoryRepository:
     case LibraryRepository:
